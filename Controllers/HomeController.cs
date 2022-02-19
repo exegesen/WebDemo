0b978304115e3e972f4e841be27428b5ec02f2a8
exegesen <86732202+exegesen@users.noreply.github.com>
2022-02-19 22:24:06 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+public class Home : Controller
+{
+    public IActionResult Index() 
+    {
+        return View("Index");
+    
+    }
+
+    public IActionResult About()
+    {
+        return View("About");
+    }
+
+    public IActionResult Contact()
+    {
+        return View("Contact");
+    }
+    public IActionResult Projects()
+    {
+        return View("Projects");
+    }
+}
