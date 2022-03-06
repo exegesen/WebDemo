1aa548d5024eed96bc85977371aaea35607fd8a3
exegesen <86732202+exegesen@users.noreply.github.com>
2022-03-06 19:15:13 +0100

    now it works

--- Controllers/NumberGuessController.cs
+++ Controllers/NumberGuessController.cs
@@ -8,20 +8,44 @@ namespace WebDemo.Controllers
     public class NumberGuess : Controller
     {
         public const string SessionKeyNumber = "_Number";
-        
+        public const string SessionWrongRightGuess = "_GuessDisplay";
+        public const string SessionHint = "_HintDisplay";
+
+        [HttpGet]
         public IActionResult Index()
         {
             
             HttpContext.Session.SetInt32(SessionKeyNumber, GuessGameFormData.generateNumber());
-            
+            HttpContext.Session.SetString(SessionWrongRightGuess, "No guess made yet!");
+
+
             return View("Index");
         }
         [HttpPost]
-        public ActionResult GuessGame(GuessGameFormData form)
+        public ActionResult Index(GuessGameFormData form)
         {
-            if (form.numberInput == HttpContext.Session.GetInt32(SessionKeyNumber)) { 
-            
+
+            if (form.numberInput > HttpContext.Session.GetInt32(SessionKeyNumber))
+            {
+
+                HttpContext.Session.SetString(SessionWrongRightGuess, "Too high!");
+                return View("Index");
             }
+            else if (form.numberInput < HttpContext.Session.GetInt32(SessionKeyNumber))
+            {
+                HttpContext.Session.SetString(SessionWrongRightGuess, "Too low!");
+                return View("Index");
+
+            }
+            if (form.numberInput == HttpContext.Session.GetInt32(SessionKeyNumber))
+            {
+                HttpContext.Session.SetString(SessionWrongRightGuess, "Correct guess!");
+            }
+            else {
+                HttpContext.Session.SetString(SessionWrongRightGuess, "Wrong guess!");
+
+            }
+
             return View("Index");
         }
     }
